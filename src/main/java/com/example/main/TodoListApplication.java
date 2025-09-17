dc22b7cfeecdc9618db431fa2851bf472ff40c9b
Ankit <ankukanyal99@gmail.com>
2025-09-17 21:29:08 +0530

    Initial commit - Spring Boot Todo App

---
+++ src/main/java/com/example/main/TodoListApplication.java
@@ -0,0 +1,19 @@
+package com.example.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@SpringBootApplication(scanBasePackages = "com.example")
+@EnableJpaRepositories(basePackages = "com.example.repository")
+@EntityScan(basePackages = "com.example.model")
+public class TodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+		System.out.print("its working");
+	}
+ 
+}
